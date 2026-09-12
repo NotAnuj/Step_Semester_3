6901fe4a91f91a9e214b869a8a97175d90300f11
NotAnuj <chanuj044@gmail.com>
2026-09-12 11:47:47 +0530

    Update main.java

--- devlops/main.java
+++ devlops/main.java
@@ -1,1 +1,19 @@
+import java.util.Scanner;
+class ClassName{
+    int id;
+    ClassName(int id){
+      this.name = name;
+    }
+    void fxnname(){
+         //body of fxn
+    }
+}
 
+public class main{
+  public static void main(String[]args){
+    Scanner sc = new Scanner(System.in);
+    // Body of main fxn
+
+    sc.close();
+  }
+}
